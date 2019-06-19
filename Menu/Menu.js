fe5908366e528dd538a5a4a5adb96d65439a6d5b
noorosce <077068@yahoo.com>
2019-06-19 17:53:53 -0500

    Added animation to menu button in Menu.js and inserted script url into index.html

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -10,3 +10,5 @@ const menu = document.querySelector('.menu')
 const menuButton = document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu)
+
+TweenMax.from('.menu-button', 2, {opacity:0, scale:0, ease:Bounce.easeOut})
